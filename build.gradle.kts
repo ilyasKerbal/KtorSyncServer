e0bba9e914240abca292a95d932ca63e207863b5
ILYAS KERBAL <kerbalsc@gmail.com>
2023-09-07 00:08:29 +0100

    Migrate Gradle builde to version catalogs: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
-
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    kotlin("jvm") version "1.9.10"
-    id("io.ktor.plugin") version "2.3.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor.plugin)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 group = "dev.appmaster"
@@ -24,16 +20,18 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-compression-jvm")
-    implementation("io.ktor:ktor-server-host-common-jvm")
-    implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-http-redirect-jvm")
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core.jvm)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.compression.jvm)
+    implementation(libs.ktor.server.host.common.jvm)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.http.redirect.jvm)
+    implementation(libs.ktor.server.auth.jvm)
+    implementation(libs.ktor.server.netty.jvm)
+    implementation(libs.logback.classic)
+
+    // Test
+    testImplementation(libs.ktor.server.tests.jvm)
+    testImplementation(libs.kotlin.test.junit)
 }
