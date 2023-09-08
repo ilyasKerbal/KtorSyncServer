4a46d073b08b8df33e6b14147676c57ab13c6d85
ILYAS KERBAL <kerbalsc@gmail.com>
2023-09-08 20:42:38 +0100

    Add configureKoin to the application

---
+++ src/main/kotlin/dev/appmaster/plugins/Koin.kt
@@ -0,0 +1,10 @@
+package dev.appmaster.plugins
+
+import dev.appmaster.core.di.configModule
+import io.ktor.server.application.*
+import org.koin.core.logger.Level
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+
+fun Application.configureKoin() {
+}
