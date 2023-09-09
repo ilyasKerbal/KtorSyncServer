8f742b6a7bd405e698f4ae5e27e1531dc8adc5e4
ILYAS KERBAL <kerbalsc@gmail.com>
2023-09-09 22:30:54 +0100

    Remove unneeded Application configuration

--- src/main/kotlin/dev/appmaster/plugins/Routing.kt
+++ src/main/kotlin/dev/appmaster/plugins/Routing.kt
@@ -8,11 +8,6 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
-        }
-    }
     routing {
         get("/") {
             call.respondText("Hello World!")
