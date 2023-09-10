7ca6902716bf162a2682680ef7a987b9b66e5214
ILYAS KERBAL <kerbalsc@gmail.com>
2023-09-10 13:06:44 +0100

    Added EndPoint sealed class

---
+++ src/main/kotlin/dev/appmaster/core/config/EndPoint.kt
@@ -0,0 +1,14 @@
+package dev.appmaster.core.config
+
+sealed class EndPoint(val path: String) {
+    // Root
+    data object Root: EndPoint(path = "/")
+
+    data object Signup: EndPoint(path = "/signup")
+
+    data object Login: EndPoint(path = "/login")
+
+    data object Check: EndPoint(path = "/check")
+
+    data object Content: EndPoint(path = "/content")
+}
