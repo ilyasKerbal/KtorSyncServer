e512fc38e5c528385398ace3fce3ac13f61315f9
ILYAS KERBAL <kerbalsc@gmail.com>
2023-09-10 13:05:01 +0100

    Add FailureMessage object

---
+++ src/main/kotlin/dev/appmaster/core/config/FailureMessages.kt
@@ -0,0 +1,9 @@
+package dev.appmaster.core.config
+
+object FailureMessages {
+    const val FAILED_MESSAGE = "Something went wrong on our end!"
+
+    const val UNAUTHORIZED_MESSAGE = "Unauthorized, you can not access this resource."
+
+    const val NOT_FOUND_MESSAGE = "Resource Not Found"
+}
