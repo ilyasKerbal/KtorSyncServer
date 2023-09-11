643abc2d1389721c84406fe2d60284344d6676a1
ILYAS KERBAL <kerbalsc@gmail.com>
2023-09-11 01:03:47 +0100

    Added JWT config on both aplication.conf and add JWTConfig to ConfigModule

--- src/main/kotlin/dev/appmaster/core/di/ConfigModule.kt
+++ src/main/kotlin/dev/appmaster/core/di/ConfigModule.kt
@@ -2,6 +2,7 @@ package dev.appmaster.core.di
 
 
 import dev.appmaster.core.config.DatabaseConfig
+import dev.appmaster.core.config.JWTConfig
 import dev.appmaster.core.config.SecretConfig
 import dev.appmaster.core.util.createConnectionString
 import io.ktor.server.application.*
@@ -36,5 +37,14 @@ fun configModule(application: Application) = module {
             .createClient(connectionString = createConnectionString(get<DatabaseConfig>()))
             .getDatabase(get<DatabaseConfig>().name)
             .coroutine
+    }
+
+    single<JWTConfig>{
+        val jwtConfig = get<ApplicationConfig>().config("jwt")
+        JWTConfig(
+            issuer = jwtConfig.property("issuer").getString(),
+            audience = jwtConfig.property("audience").getString(),
+            claim = jwtConfig.property("claim").getString()
+        )
     }
 }
